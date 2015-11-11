31782870171cf68e3667a090d8ac5992aaf70137
JRice <jrice@eol.org>
2015-11-11 15:04:30 -0500

    Create project.

---
+++ Harvester/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Harvester::Application.config.secret_token = 'aa48f23c1710b2f23f2dc6e4f0b4ed3e87db4a3ce6eb1bd97149384cb885cff8a1e40cdedbecdaeae6fe72cee479b513f7e6b4f370d7c1057647494d6afafef4'
