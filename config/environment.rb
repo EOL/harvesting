d3f8455ca3932fe177422b971c75180919236ede
JRice <jrice@eol.org>
2015-11-12 10:27:52 -0500

    try that again

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,5 @@
-# Load the rails application
+# Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
-# Initialize the rails application
-Harvester::Application.initialize!
-
-Rails.configuration.site_id = ENV["SITE_ID"] || 1
+# Initialize the Rails application.
+Rails.application.initialize!
