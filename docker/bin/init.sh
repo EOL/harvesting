d7d9ee47732cc612457e053d496a48b210dfdca6
m3hran <m3hran@gmail.com>
2017-10-04 11:09:27 -0400

    staging env retry

--- docker/bin/init.sh
+++ docker/bin/init.sh
@@ -4,6 +4,5 @@ APP="harvester"
 echo "Updating $APP.."
 cd /u/apps/$APP && git pull
 
-rm -f /u/app/$APP/Gemfile.lock
 /bin/bash -l -c 'cd /u/apps/harvester/ && bundle' 
 /bin/bash -l -c 'cd /u/apps/harvester/ && rake log:clear && rails s -d -b 0.0.0.0' 
