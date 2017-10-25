9b7797a1ab5df8ee59a7d1828b2cd8f51eb45ef4
m3hran <m3hran@gmail.com>
2017-10-25 11:10:17 -0400

    added gnparser & based on ruby 2.4.2

--- docker/bin/init.sh
+++ docker/bin/init.sh
@@ -2,7 +2,7 @@
 APP="harvester"
 
 echo "Updating $APP.."
-cd /u/apps/$APP && git pull
+cd /u/apps/$APP && git pull && git checkout GEMFILE.lock
 
 /bin/bash -l -c 'cd /u/apps/harvester/ && bundle' 
 /bin/bash -l -c 'cd /u/apps/harvester/ && rake log:clear && rails s -d -b 0.0.0.0' 
