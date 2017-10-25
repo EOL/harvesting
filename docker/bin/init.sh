1878e6bbaff86a896fc176e600048b2264c25dd2
m3hran <m3hran@gmail.com>
2017-10-25 11:27:53 -0400

    self update bundler

--- docker/bin/init.sh
+++ docker/bin/init.sh
@@ -4,5 +4,6 @@ APP="harvester"
 echo "Updating $APP.."
 cd /u/apps/$APP && git pull && git checkout GEMFILE.lock
 
+/bin/bash -l -c 'gem install bundler --pre'
 /bin/bash -l -c 'cd /u/apps/harvester/ && bundle' 
 /bin/bash -l -c 'cd /u/apps/harvester/ && rake log:clear && rails s -d -b 0.0.0.0' 
