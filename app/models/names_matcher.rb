6bf1dc6107ae286a2c79f30b8a9ed9db0bed3db7
JRice <jrice@eol.org>
2017-11-01 10:45:53 -0400

    Improve output of unmatched nodes

--- app/models/names_matcher.rb
+++ app/models/names_matcher.rb
@@ -263,8 +263,10 @@ class NamesMatcher
 
   def log_unmatched
     return if @unmatched.blank?
-    @unmatched.in_groups_of(100, false) do |group|
-      @harvest.log("Unmatched nodes: #{group.join('; ')}", cat: :warns)
+    if @unmatched.size > 100
+      @harvest.log("#{@unmatched.size} Unmatched nodes! That's too many to output. First 50: #{@unmatched[0..50].join('; ')}", cat: :warns)
+    else
+      @harvest.log("Unmatched nodes (#{@unmatched.size}): #{@unmatched.join('; ')}", cat: :warns)
     end
   end
 end
