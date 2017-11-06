c5502fd8de97af1e78fe7188426d659f81d26364
JRice <jrice@eol.org>
2017-11-06 10:30:32 -0500

    More output for flattener

--- app/models/flattener.rb
+++ app/models/flattener.rb
@@ -61,6 +61,7 @@ class Flattener
   def update_tables
     NodeAncestor.where(resource_id: @resource.id).delete_all
     # TODO: error-handling
+    puts("Flattening #{@node_ancestors.size} ancestors")
     NodeAncestor.import! @node_ancestors
     NodeAncestor.propagate_id(fk: 'ancestor_id', other: 'nodes.id', set: 'ancestor_fk', with: 'resource_pk')
   end
