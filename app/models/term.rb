6c735803b72e800f9ef300d15d8594f15108493a
JRice <jrice@eol.org>
2017-12-09 07:40:34 -0500

    Update terms, allow re-running Term.from_json

--- app/models/term.rb
+++ app/models/term.rb
@@ -3,6 +3,9 @@ class Term < ActiveRecord::Base
 
   def self.from_json(json)
     terms = []
+    existing_terms = {}
+    # NOTE: I'm not terribly worried about performance for this:
+    Term.find_each { |t| existing_terms[t.uri] = t.id }
     json.each do |u|
       u['used_for'] = u.delete('type')
       u.delete('hide_from_gui') # unused
@@ -10,12 +13,17 @@ class Term < ActiveRecord::Base
       u['is_hidden_from_glossary'] = u.delete('hide_from_glossary')
       u['is_text_only'] = u.delete('value_is_text')
       u['is_verbatim_only'] = u.delete('value_is_verbatim')
+      u['id'] = existing_terms[u['uri']] if existing_terms.key?(u['uri']) # Will update
       terms << u
     end
     terms.in_groups_of(2000, false) do |group|
-      Term.import(group)
+      Term.import(
+        group,
+        on_duplicate_key_update: %i[uri name definition comment attribution is_hidden_from_glossary
+                                    is_hidden_from_overview ontology_information_url ontology_source_url is_text_only
+                                    is_verbatim_only position used_for]
+      )
     end
     terms
   end
-
 end
