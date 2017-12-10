5ff74d2c144119b00c919c64b5fcb3921444598e
JRice <jrice@eol.org>
2017-12-10 09:36:18 -0500

    Should fix associations controller query

--- app/controllers/assocs_controller.rb
+++ app/controllers/assocs_controller.rb
@@ -1,8 +1,11 @@
 class AssocsController < ApplicationController
   def index
     @resource = Resource.find(params[:resource_id])
+
     @assocs = prep_for_api(@resource.assocs.published
-                           .includes(:predicate_term, :sex_term, :lifestage_term, :assocs_references,
+                           .includes(:predicate_term, :sex_term, :lifestage_term,
+                                     assocs_references: :references,
+                                     occurrence: { occurrence_metadata: %i[predicate_term object_term] },
                                      node: :scientific_name, target_node: :scientific_name,
                                      meta_assocs: %i[predicate_term object_term units_term statistical_method_term]))
     respond_to do |fmt|
