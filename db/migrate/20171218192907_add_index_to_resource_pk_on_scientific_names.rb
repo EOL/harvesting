9ee712d576997086fe9d6e609a6d51d667ad1cfa
JRice <jrice@eol.org>
2017-12-18 14:32:47 -0500

    Need to index that new field on S_names; also give a reasonable default value to existing rows' '

---
+++ db/migrate/20171218192907_add_index_to_resource_pk_on_scientific_names.rb
@@ -0,0 +1,8 @@
+class AddIndexToResourcePkOnScientificNames < ActiveRecord::Migration
+  def change
+    # This is a resonable default, for things that HAD been harvested... it will screw up synonyms, but there's no way
+    # to fix without reharvesting them:
+    ScientificName.where('resource_pk IS NULL').update_all('resource_pk = node_resource_pk')
+    add_index :scientific_names, :resource_pk
+  end
+end
