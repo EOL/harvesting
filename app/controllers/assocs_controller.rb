e50d506778593d0d0c78000f11285f175efa301d
JRice <jrice@eol.org>
2017-12-19 12:57:17 -0500

    Fixes a bug with slow assoc pages

--- app/controllers/assocs_controller.rb
+++ app/controllers/assocs_controller.rb
@@ -3,8 +3,7 @@ class AssocsController < ApplicationController
     @resource = Resource.find(params[:resource_id])
 
     @assocs = prep_for_api(@resource.assocs.published
-                           .includes(:predicate_term, :sex_term, :lifestage_term,
-                                     assocs_references: :reference,
+                           .includes(:predicate_term, :sex_term, :lifestage_term, :references,
                                      occurrence: { occurrence_metadata: %i[predicate_term object_term] },
                                      node: :scientific_name, target_node: :scientific_name,
                                      meta_assocs: %i[predicate_term object_term units_term statistical_method_term]))
