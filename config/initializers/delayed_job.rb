6e7497bdad6931d23aa66601ffe675bb8e5ae1d9
JRice <jrice@eol.org>
2018-02-15 09:56:12 -0500

    Configure delayed job

--- config/initializers/delayed_job.rb
+++ config/initializers/delayed_job.rb
@@ -4,22 +4,20 @@ Delayed::Worker.queue_attributes = {
   media: { priority: 10 },
   harvest: { priority: 0 }
 }
-Delayed::Worker.max_run_time = 48.hours # Yes, really. ...and I hope that's enough.
+Delayed::Worker.max_run_time = 7.days # Yes, really. We watch the long-running jobs pretty closely.
 Delayed::Worker.max_attempts = 2
 
-# TODO: move to jobs folder?
-# module Harvester
-  HarvestJob = Struct.new(:resource_id) do
-    def perform
-      Resource.find(resource_id).harvest
-    end
-
-    def queue_name
-      'harvest'
-    end
-
-    def max_attempts
-      1
-    end
+# NOTE: If you add another one of these, you should really move them to a jobs folder.
+HarvestJob = Struct.new(:resource_id) do
+  def perform
+    Resource.find(resource_id).harvest
   end
-# end
+
+  def queue_name
+    'harvest'
+  end
+
+  def max_attempts
+    1
+  end
+end
