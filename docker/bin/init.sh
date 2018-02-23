8856b959ddefd0d5380613032bbb75c1e70f896a
JRice <jrice@eol.org>
2018-02-23 13:30:36 -0500

    Restart nginx after unicorn starts

--- docker/bin/init.sh
+++ docker/bin/init.sh
@@ -8,3 +8,4 @@ cd /u/apps/$APP && git pull
 /bin/bash -l -c 'cd /u/apps/harvester/ && bundle'
 /bin/bash -l -c 'service nginx start'
 /bin/bash -l -c 'unicorn -c /u/apps/harvester/config/unicorn.rb -E staging -D'
+/bin/bash -l -c 'service nginx restart'
