be23d26489cbe7969c380238819fa2333b5e2a11
JRice <jrice@eol.org>
2018-02-23 10:47:48 -0500

    Improve opendata import

--- app/models/resource/from_open_data.rb
+++ app/models/resource/from_open_data.rb
@@ -8,6 +8,65 @@ class Resource
       # E.g.: https://opendata.eol.org/dataset/anage/resource/2af10fa0-db2a-4908-bc85-505f691419dd
       @url = url
       @partner = nil
+      @abbreviations = {
+        america: 'am',
+        american: 'am',
+        arctic: 'arc',
+        atlas: 'atl',
+        australia: 'aus',
+        australian: 'aus',
+        biology: 'bio',
+        catalog: 'cat',
+        checklist: 'chk',
+        checklists: 'chk',
+        college: 'c',
+        data: 'dat',
+        database: 'db',
+        distribution: 'dist',
+        diversity: 'div',
+        ecology: 'eco',
+        ecological: 'eco',
+        encyclopedia: 'enc',
+        habitat: 'hab',
+        harvard: 'hvd',
+        hierarchy: 'hier',
+        images: 'imgs',
+        international: 'intl',
+        interactions: 'int',
+        journal: 'j',
+        living: 'liv',
+        marine: 'mar',
+        measurements: 'meas',
+        national: 'ntl',
+        naturalist: 'nat',
+        nature: 'nat',
+        ocean: 'oc',
+        oceans: 'oc',
+        oceanic: 'oc',
+        pictures: 'pics',
+        plant: 'pl',
+        plants: 'pl',
+        public: 'pub',
+        record: 'rec',
+        records: 'rec',
+        register: 'reg',
+        smithsonian: 'si',
+        species: 'sp',
+        states: 's',
+        structured: 'struct',
+        summary: 'sum',
+        test: 'tst',
+        text: 'txt',
+        university: 'u',
+        universities: 'u',
+        united: 'u',
+        unitedstates: 'us',
+        video: 'vid',
+        videos: 'vid',
+        wikimedia: 'wiki',
+        wikipedia: 'wiki'
+      }
+      @stopwords = %w[a about all are an and be by do know or of on out for in is the this to was with what]
     end
 
     def parse
@@ -42,7 +101,7 @@ class Resource
     def create_resource(noko)
       name = strip_string(noko.css('h1').first.text)
       # Lots of names are super-lame. In those cases, let's just re-use the partner name:
-      name = strip_string(noko.css('.breadcrumb li a')[-2].text) if name.match?(/^Resource/)
+      name = strip_string(noko.css('.breadcrumb li a')[-2].text) if name.match?(/resource/i)
       abbr = abbreviate(name)
       desc = noko.css('.prose p,blockquote').map(&:text).map { |txt| strip_string(txt) }.join("\n")
       @resource = Resource.create(name: name, abbr: abbr, description: desc, notes: 'auto-harvested, requires editing.',
@@ -84,16 +143,19 @@ class Resource
     end
 
     def strip_string(str)
-      str.gsub(/\W+/m, ' ').gsub(/^\s+/m, '').gsub(/\s+$/m, '')
+      str.gsub(/\s*\(\d+\)\s*/m, '').gsub(/\W+/m, ' ').gsub(/^\s+/m, '').gsub(/\s+$/m, '')
     end
 
     def abbreviate(name)
       return name if name.size < 8
-      words = name.split
+      words = name.split.map { |n| n.downcase }
+      words.delete_if { |w| @stopwords.include?(w) }
+      words.map do |word|
+        sym = word.to_sym
+        @abbreviations.key?(sym) ? @abbreviations[sym] : word
+      end
       abbr = if words.size > 4
         words.map { |w| w.first }.join[0..15]
-      elsif name.sub(/[^A-Z]/, '').size >= 3
-        name.sub(/[^A-Z]/, '')[0..15]
       else
         name[0..15]
       end
