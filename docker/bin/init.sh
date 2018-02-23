211783e2e38a300e0d1d7b83b4015e41245f32d5
JRice <jrice@eol.org>
2018-02-23 12:27:28 -0500

    Stop starting thin, start unicorn and nginx.

--- docker/bin/init.sh
+++ docker/bin/init.sh
@@ -5,5 +5,6 @@ echo "Updating $APP.."
 cd /u/apps/$APP && git pull
 
 /bin/bash -l -c 'gem install bundler --pre'
-/bin/bash -l -c 'cd /u/apps/harvester/ && bundle' 
-/bin/bash -l -c 'cd /u/apps/harvester/ && rake log:clear && rails s -d -b 0.0.0.0' 
+/bin/bash -l -c 'cd /u/apps/harvester/ && bundle'
+/bin/bash -l -c 'service nginx start'
+/bin/bash -l -c 'unicorn -c /u/apps/harvester/config/unicorn.rb -E staging -D'
