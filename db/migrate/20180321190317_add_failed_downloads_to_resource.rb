c717a02041f92f4493b81f0aa5259fe60985bb27
JRice <jrice@eol.org>
2018-03-21 15:33:52 -0400

    Redo migration

--- db/migrate/20180321190317_add_failed_downloads_to_resource.rb
+++ db/migrate/20180321190317_add_failed_downloads_to_resource.rb
@@ -1,7 +1,8 @@
+# 20180321190317
 class AddFailedDownloadsToResource < ActiveRecord::Migration
   def change
-    add_column :resources, :downloaded_media_count, :integer, comment: 'how many images have successfully downloaded.'
-    add_column :resources, :failed_downloaded_media_count, :integer,
+    add_column :resources, :downloaded_media_count, :integer, default: 0, comment: 'how many images have successfully downloaded.'
+    add_column :resources, :failed_downloaded_media_count, default: 0, :integer,
                comment: 'how many images have NOT successfully downloaded.'
   end
 end
