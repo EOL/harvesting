91c45d7e16860f5193333010768eeffada791821
JRice <jrice@eol.org>
2018-03-21 16:08:24 -0400

    Ugh. Wrird bug fixed

--- app/models/medium.rb
+++ app/models/medium.rb
@@ -24,7 +24,7 @@ class Medium < ActiveRecord::Base
     attr_accessor :sizes, :bucket_size
 
     def download_and_resize(images)
-      images.pluck(&:id).each { |img_id| Delayed::Job.enqueue(DownloadMediumJob.new(img_id)) }
+      images.select('id').map(&:id).each { |img_id| Delayed::Job.enqueue(DownloadMediumJob.new(img_id)) }
     end
   end
 
