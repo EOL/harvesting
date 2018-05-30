e7fbca01ac524694be33e66a3efd42c6fb3da9f9
JRice <jrice@eol.org>
2018-05-30 13:29:41 -0400

    Fixing a nil case in checking uris.

--- app/models/term.rb
+++ app/models/term.rb
@@ -2,8 +2,10 @@ class Term < ActiveRecord::Base
   enum used_for: %i[unknown measurement association value metadata]
 
   def self.uri?(uri)
+    return false if uri.nil?
+    return false unless uri.respond_to?(:=~) # String-ish duck type
     @valid_protocols ||= %w[http doi].join('|')
-    return false unless (uri =~ URI::ABS_URI).zero? # NOTE: must be at the start
+    return false unless (uri =~ URI::ABS_URI)&.zero? # NOTE: must be at the start
     return false unless uri =~ /^(#{@valid_protocols})/i
     true
   end
