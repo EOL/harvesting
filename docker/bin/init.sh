1b2852a8d382de5d8ab503e40378a36617cda6f5
JRice <jrice@eol.org>
2018-07-12 10:21:39 -0400

    Commenting out nginx in the init script. Argh.

--- docker/bin/init.sh
+++ docker/bin/init.sh
@@ -8,6 +8,6 @@ cp /u/apps/secrets.yml /u/apps/$APP/config/secrets.yml
 
 /bin/bash -l -c 'gem install bundler --pre'
 /bin/bash -l -c 'cd /u/apps/harvester/ && bundle'
-/bin/bash -l -c 'service nginx start'
-/bin/bash -l -c 'unicorn -c /u/apps/harvester/config/unicorn.rb -E staging -D'
-/bin/bash -l -c 'service nginx restart'
+# /bin/bash -l -c 'service nginx start'
+# /bin/bash -l -c 'unicorn -c /u/apps/harvester/config/unicorn.rb -E staging -D'
+# /bin/bash -l -c 'service nginx restart'
