bada5af887a3c727338386168e2be7420ae3ea4f
JRice <jrice@eol.org>
2018-10-10 16:34:18 -0400

    Adding official migration for manual change

--- db/migrate/20181010193307_articles_have_medium_text.rb
+++ db/migrate/20181010193307_articles_have_medium_text.rb
@@ -1,5 +1,11 @@
 class ArticlesHaveMediumText < ActiveRecord::Migration
   def change
     change_column :articles, :body, :text, limit: 16.megabytes - 1
+    Article.connection.execute(%{
+      ALTER TABLE articles CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_bin,
+      MODIFY body mediumtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin,
+      MODIFY `owner` text CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+      MODIFY `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL;
+    })
   end
 end
