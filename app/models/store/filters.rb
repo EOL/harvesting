7ae8d8af74069309a3159d10d4d6a9a77f60c52c
JRice <jrice@eol.org>
2019-04-19 14:06:43 -0400

    Just adding a comment...

--- app/models/store/filters.rb
+++ app/models/store/filters.rb
@@ -1,4 +1,5 @@
 module Store
+  # These are just methods that can be called by several other "Store" modules.
   module Filters
     def remove_emojis(val)
       val.gsub(Unicode::Emoji::REGEX, '')
