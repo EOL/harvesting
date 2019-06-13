f6adf2f85d1574027aa675785ad7a7f7b9640919
JRice <jrice@eol.org>
2019-06-13 12:09:26 -0400

    Oops, .export_ancestry was OUTSIDE of NodeAncestor class...

--- app/models/node_ancestor.rb
+++ app/models/node_ancestor.rb
@@ -3,15 +3,16 @@ class NodeAncestor < ActiveRecord::Base
   belongs_to :resource, inverse_of: :node_ancestors
   belongs_to :node, inverse_of: :node_ancestors
   belongs_to :ancestor, class_name: 'Node', inverse_of: :descendants
-end
 
-def self.export_ancestry(resource)
-  CSV.open(resource.path.join('publish_node_ancestors'), 'ab') do |csv|
-    where(resource_id: resource.id).includes(:node, :ancestor).find_each do |nod_anc|
-      csv << [nod_anc.node.page_id, nod_anc.ancestor.page_id] if nod_anc.node&.page_id && nod_anc.ancestor&.page_id
+  def self.export_ancestry(resource)
+    CSV.open(resource.path.join('publish_node_ancestors'), 'ab') do |csv|
+      where(resource_id: resource.id).includes(:node, :ancestor).find_each do |nod_anc|
+        csv << [nod_anc.node.page_id, nod_anc.ancestor.page_id] if nod_anc.node&.page_id && nod_anc.ancestor&.page_id
+      end
     end
   end
 end
+
 
 # node_ancestors:
 #   node_id: 2
