eebfb7195f2cb9fc039097c3f2c463faaeddc5b2
JRice <jrice@eol.org>
2020-03-10 15:45:52 -0400

    After initial upgrade.

--- config/environment.rb
+++ config/environment.rb
@@ -1,10 +1,10 @@
 # Load the Rails application.
-require File.expand_path('../application', __FILE__)
+require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
 
 # Cleaner (and less shout-y) reading from the environment variables:
 def env(name, default)
-  ENV["EOL_#{name.upcase}"] || default
+ ENV["EOL_#{name.upcase}"] || default
 end
