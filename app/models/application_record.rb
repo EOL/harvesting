ee2ef7742779b1a0da0428cd2873369a6d934cd4
JRice <jrice@eol.org>
2020-03-19 08:17:43 -0400

    Initiaslizer didnt work.

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,7 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+  # Turning off this "protection" because harvesting ALMOST ALWAYS has fields that *look* required, but are infact allowed
+  # as nulls because we have to populate the fields in a second pass (using a resource_pk to look up the ID in the DB).
+  # It's just too much of a hassle to account for this is every case where it's needed.
+  self.belongs_to_required_by_default = false
 end
--- config/initializers/new_framework_defaults_5_2.rb
+++
@@ -1,7 +0,0 @@
-# Make `form_with` generate id attributes for any generated HTML tags.
-# Rails.application.config.action_view.form_with_generates_ids = true
-
-# Turning off this "protection" because harvesting ALMOST ALWAYS has fields that *look* required, but are infact allowed
-# as nulls because we have to populate the fields in a second pass (using a resource_pk to look up the ID in the DB).
-# It's just too much of a hassle to account for this is every case where it's needed.
-Rails.application.config.active_record.belongs_to_required_by_default = false
