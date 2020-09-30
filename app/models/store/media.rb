39349f5fe9acb18a690c126bbdd75df801bb733b
Michael Vitale <mmvitale@gmail.com>
2020-09-30 11:38:24 -0400

    use :ogg format for videos and audio

--- app/models/store/media.rb
+++ app/models/store/media.rb
@@ -63,7 +63,7 @@ module Store
         'audio/mpeg' => :mp3, # NOTE: this one is "best".
         'audio/mp3' => :mp3,
         'audio/ogg' => :ogg, # NOTE: this one is "best"
-        'video/ogg' => :ogv,
+        'video/ogg' => :ogg,
         'application/ogg' => :ogg,
         'audio/wav' => :wav,
         'audio/x-wav' => :wav,
