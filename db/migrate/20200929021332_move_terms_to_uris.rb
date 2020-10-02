9206a14882db7b9dbb934083963a949f0559e089
JRice <jrice@eol.org>
2020-10-02 14:01:56 -0400

    ANOTHER fix for beta

--- db/migrate/20200929021332_move_terms_to_uris.rb
+++ db/migrate/20200929021332_move_terms_to_uris.rb
@@ -13,54 +13,57 @@ class MoveTermsToUris < ActiveRecord::Migration[5.2]
       term_uris[term.uri] = term.id
     end
 
-    # add_column :assoc_traits, :predicate_term_uri, :string
-    # add_column :assoc_traits, :object_term_uri, :string
-    # add_column :assoc_traits, :units_term_uri, :string
-    add_column :assoc_traits, :statistical_method_term_uri, :string
-    #
-    # add_column :assocs, :predicate_term_uri, :string
-    # add_column :assocs, :sex_term_uri, :string
-    # add_column :assocs, :lifestage_term_uri, :string
-    #
-    # add_column :occurrence_metadata, :predicate_term_uri, :string
-    # add_column :occurrence_metadata, :object_term_uri, :string
-    # add_column :occurrence_metadata, :units_term_uri, :string
-    add_column :occurrence_metadata, :statistical_method_term_uri, :string
-    #
-    # add_column :traits, :predicate_term_uri, :string
-    # add_column :traits, :object_term_uri, :string
-    # add_column :traits, :units_term_uri, :string
-    add_column :traits, :statistical_method_term_uri, :string
-    # add_column :traits, :sex_term_uri, :string
-    # add_column :traits, :lifestage_term_uri, :string
+    begin
+      # add_column :assoc_traits, :predicate_term_uri, :string
+      # add_column :assoc_traits, :object_term_uri, :string
+      # add_column :assoc_traits, :units_term_uri, :string
+      # add_column :assoc_traits, :statistical_method_term_uri, :string
+      #
+      # add_column :assocs, :predicate_term_uri, :string
+      # add_column :assocs, :sex_term_uri, :string
+      # add_column :assocs, :lifestage_term_uri, :string
+      #
+      # add_column :occurrence_metadata, :predicate_term_uri, :string
+      # add_column :occurrence_metadata, :object_term_uri, :string
+      # add_column :occurrence_metadata, :units_term_uri, :string
+      # add_column :occurrence_metadata, :statistical_method_term_uri, :string
+      #
+      # add_column :traits, :predicate_term_uri, :string
+      # add_column :traits, :object_term_uri, :string
+      # add_column :traits, :units_term_uri, :string
+      # add_column :traits, :statistical_method_term_uri, :string
+      # add_column :traits, :sex_term_uri, :string
+      # add_column :traits, :lifestage_term_uri, :string
 
-    term_uris.each do |uri, id|
-      AssocTrait.where(predicate_term_id: id).update_all(predicate_term_uri: uri)
-      AssocTrait.where(object_term_id: id).update_all(object_term_uri: uri)
-      AssocTrait.where(units_term_id: id).update_all(units_term_uri: uri)
-      AssocTrait.where(statistical_method_term_id: id).update_all(statistical_method_term_uri: uri)
-      Assoc.where(predicate_term_id: id).update_all(predicate_term_uri: uri)
-      Assoc.where(sex_term_id: id).update_all(sex_term_uri: uri)
-      Assoc.where(lifestage_term_id: id).update_all(lifestage_term_uri: uri)
-      OccurrenceMetadata.where(predicate_term_id: id).update_all(predicate_term_uri: uri)
-      OccurrenceMetadata.where(object_term_id: id).update_all(object_term_uri: uri)
-      OccurrenceMetadata.where(units_term_id: id).update_all(units_term_uri: uri)
-      OccurrenceMetadata.where(statistical_method_term_id: id).update_all(statistical_method_term_uri: uri)
-      Trait.where(predicate_term_id: id).update_all(predicate_term_uri: uri)
-      Trait.where(object_term_id: id).update_all(object_term_uri: uri)
-      Trait.where(units_term_id: id).update_all(units_term_uri: uri)
-      Trait.where(statistical_method_term_id: id).update_all(statistical_method_term_uri: uri)
-      Trait.where(sex_term_id: id).update_all(sex_term_uri: uri)
-      Trait.where(lifestage_term_id: id).update_all(lifestage_term_uri: uri)
-    end
+      term_uris.each do |uri, id|
+        # AssocTrait.where(predicate_term_id: id).update_all(predicate_term_uri: uri)
+        # AssocTrait.where(object_term_id: id).update_all(object_term_uri: uri)
+        # AssocTrait.where(units_term_id: id).update_all(units_term_uri: uri)
+        # AssocTrait.where(statistical_method_term_id: id).update_all(statistical_method_term_uri: uri)
+        # Assoc.where(predicate_term_id: id).update_all(predicate_term_uri: uri)
+        # Assoc.where(sex_term_id: id).update_all(sex_term_uri: uri)
+        # Assoc.where(lifestage_term_id: id).update_all(lifestage_term_uri: uri)
+        OccurrenceMetadatum.where(predicate_term_id: id).update_all(predicate_term_uri: uri)
+        OccurrenceMetadatum.where(object_term_id: id).update_all(object_term_uri: uri)
+        OccurrenceMetadatum.where(units_term_id: id).update_all(units_term_uri: uri)
+        OccurrenceMetadatum.where(statistical_method_term_id: id).update_all(statistical_method_term_uri: uri)
+        Trait.where(predicate_term_id: id).update_all(predicate_term_uri: uri)
+        Trait.where(object_term_id: id).update_all(object_term_uri: uri)
+        Trait.where(units_term_id: id).update_all(units_term_uri: uri)
+        Trait.where(statistical_method_term_id: id).update_all(statistical_method_term_uri: uri)
+        Trait.where(sex_term_id: id).update_all(sex_term_uri: uri)
+        Trait.where(lifestage_term_id: id).update_all(lifestage_term_uri: uri)
+      end
 
-    # Aaaaaactually, I think I'll delete the _ids in a second migration after I'm happy with these.
+      # Aaaaaactually, I think I'll delete the _ids in a second migration after I'm happy with these.
 
-    unless bad_uris.empty?
-      puts 'THERE WERE Terms WHICH ARE NOT RECOGNIZED:'
-      puts 'It is NOT okay to publish resources that use these terms, you will have to manually handle them.'
-      bad_uris.each do |uri, id|
-        puts "#{uri} { Term.find(#{id}) }"
+    ensure
+      unless bad_uris.empty?
+        puts 'THERE WERE Terms WHICH ARE NOT RECOGNIZED:'
+        puts 'It is NOT okay to publish resources that use these terms, you will have to manually handle them.'
+        bad_uris.each do |uri, id|
+          puts "#{uri} { Term.find(#{id}) }"
+        end
       end
     end
   end
