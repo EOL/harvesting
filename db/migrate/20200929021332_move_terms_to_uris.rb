867065e14147c951f31718518802d842bbe8e1bf
JRice <jrice@eol.org>
2020-10-07 16:07:36 -0400

    restoring the migration for running in prod (or elsewhere)

--- db/migrate/20200929021332_move_terms_to_uris.rb
+++ db/migrate/20200929021332_move_terms_to_uris.rb
@@ -14,76 +14,76 @@ class MoveTermsToUris < ActiveRecord::Migration[5.2]
     end
 
     begin
-      # add_column :assoc_traits, :predicate_term_uri, :string
-      # add_column :assoc_traits, :object_term_uri, :string
-      # add_column :assoc_traits, :units_term_uri, :string
-      # add_column :assoc_traits, :statistical_method_term_uri, :string
-      #
-      # add_column :assocs, :predicate_term_uri, :string
-      # add_column :assocs, :sex_term_uri, :string
-      # add_column :assocs, :lifestage_term_uri, :string
-      #
-      # add_column :occurrence_metadata, :predicate_term_uri, :string
-      # add_column :occurrence_metadata, :object_term_uri, :string
-      # add_column :occurrence_metadata, :units_term_uri, :string
-      # add_column :occurrence_metadata, :statistical_method_term_uri, :string
-      #
-      # add_column :traits, :predicate_term_uri, :string
-      # add_column :traits, :object_term_uri, :string
-      # add_column :traits, :units_term_uri, :string
-      # add_column :traits, :statistical_method_term_uri, :string
-      # add_column :traits, :sex_term_uri, :string
-      # add_column :traits, :lifestage_term_uri, :string
+      add_column :assoc_traits, :predicate_term_uri, :string
+      add_column :assoc_traits, :object_term_uri, :string
+      add_column :assoc_traits, :units_term_uri, :string
+      add_column :assoc_traits, :statistical_method_term_uri, :string
 
-      # add_column :meta_traits, :predicate_term_uri, :string
-      # add_column :meta_traits, :object_term_uri, :string
-      # add_column :meta_traits, :units_term_uri, :string
-      # add_column :meta_traits, :statistical_method_term_uri, :string
+      add_column :assocs, :predicate_term_uri, :string
+      add_column :assocs, :sex_term_uri, :string
+      add_column :assocs, :lifestage_term_uri, :string
+
+      add_column :occurrence_metadata, :predicate_term_uri, :string
+      add_column :occurrence_metadata, :object_term_uri, :string
+      add_column :occurrence_metadata, :units_term_uri, :string
+      add_column :occurrence_metadata, :statistical_method_term_uri, :string
+
+      add_column :traits, :predicate_term_uri, :string
+      add_column :traits, :object_term_uri, :string
+      add_column :traits, :units_term_uri, :string
+      add_column :traits, :statistical_method_term_uri, :string
+      add_column :traits, :sex_term_uri, :string
+      add_column :traits, :lifestage_term_uri, :string
+
+      add_column :meta_traits, :predicate_term_uri, :string
+      add_column :meta_traits, :object_term_uri, :string
+      add_column :meta_traits, :units_term_uri, :string
+      add_column :meta_traits, :statistical_method_term_uri, :string
 
       puts "#{term_uris.size} URIs to update..."
-      # print "AssocTrait."
-      # AssocTrait.propagate_id(fk: 'predicate_term_id', other: 'terms.id', set: 'predicate_term_uri', with: 'uri')
-      # print '.'
-      # AssocTrait.propagate_id(fk: 'object_term_id', other: 'terms.id', set: 'object_term_uri', with: 'uri')
-      # print '.'
-      # AssocTrait.propagate_id(fk: 'units_term_id', other: 'terms.id', set: 'units_term_uri', with: 'uri')
-      # print '.'
-      # AssocTrait.propagate_id(fk: 'statistical_method_term_id', other: 'terms.id', set: 'statistical_method_term_uri', with: 'uri')
-      # print 'Assoc.'
-      # Assoc.propagate_id(fk: 'predicate_term_id', other: 'terms.id', set: 'predicate_term_uri', with: 'uri')
-      # print '.'
-      # Assoc.propagate_id(fk: 'sex_term_id', other: 'terms.id', set: 'sex_term_uri', with: 'uri')
-      # print '.'
-      # Assoc.propagate_id(fk: 'lifestage_term_id', other: 'terms.id', set: 'lifestage_term_uri', with: 'uri')
-      # print 'OccMeta.'
-      # OccurrenceMetadatum.propagate_id(fk: 'predicate_term_id', other: 'terms.id', set: 'predicate_term_uri', with: 'uri')
-      # print '.'
-      # OccurrenceMetadatum.propagate_id(fk: 'object_term_id', other: 'terms.id', set: 'object_term_uri', with: 'uri')
-      # print '.'
-      # OccurrenceMetadatum.propagate_id(fk: 'units_term_id', other: 'terms.id', set: 'units_term_uri', with: 'uri')
-      # print '.'
-      # OccurrenceMetadatum.propagate_id(fk: 'statistical_method_term_id', other: 'terms.id', set: 'statistical_method_term_uri', with: 'uri')
-      # print 'Trait.'
-      # Trait.propagate_id(fk: 'predicate_term_id', other: 'terms.id', set: 'predicate_term_uri', with: 'uri')
-      # print '.'
-      # Trait.propagate_id(fk: 'object_term_id', other: 'terms.id', set: 'object_term_uri', with: 'uri')
-      # print '.'
-      # Trait.propagate_id(fk: 'units_term_id', other: 'terms.id', set: 'units_term_uri', with: 'uri')
-      # print '.'
-      # Trait.propagate_id(fk: 'statistical_method_term_id', other: 'terms.id', set: 'statistical_method_term_uri', with: 'uri')
-      # print '.'
-      # Trait.propagate_id(fk: 'sex_term_id', other: 'terms.id', set: 'sex_term_uri', with: 'uri')
-      # print '.'
-      # Trait.propagate_id(fk: 'lifestage_term_id', other: 'terms.id', set: 'lifestage_term_uri', with: 'uri')
-      # puts "\n"
+      print "AssocTrait."
+      AssocTrait.propagate_id(fk: 'predicate_term_id', other: 'terms.id', set: 'predicate_term_uri', with: 'uri')
+      print '.'
+      AssocTrait.propagate_id(fk: 'object_term_id', other: 'terms.id', set: 'object_term_uri', with: 'uri')
+      print '.'
+      AssocTrait.propagate_id(fk: 'units_term_id', other: 'terms.id', set: 'units_term_uri', with: 'uri')
+      print '.'
+      AssocTrait.propagate_id(fk: 'statistical_method_term_id', other: 'terms.id', set: 'statistical_method_term_uri', with: 'uri')
+      print 'Assoc.'
+      Assoc.propagate_id(fk: 'predicate_term_id', other: 'terms.id', set: 'predicate_term_uri', with: 'uri')
+      print '.'
+      Assoc.propagate_id(fk: 'sex_term_id', other: 'terms.id', set: 'sex_term_uri', with: 'uri')
+      print '.'
+      Assoc.propagate_id(fk: 'lifestage_term_id', other: 'terms.id', set: 'lifestage_term_uri', with: 'uri')
+      print 'OccMeta.'
+      OccurrenceMetadatum.propagate_id(fk: 'predicate_term_id', other: 'terms.id', set: 'predicate_term_uri', with: 'uri')
+      print '.'
+      OccurrenceMetadatum.propagate_id(fk: 'object_term_id', other: 'terms.id', set: 'object_term_uri', with: 'uri')
+      print '.'
+      OccurrenceMetadatum.propagate_id(fk: 'units_term_id', other: 'terms.id', set: 'units_term_uri', with: 'uri')
+      print '.'
+      OccurrenceMetadatum.propagate_id(fk: 'statistical_method_term_id', other: 'terms.id', set: 'statistical_method_term_uri', with: 'uri')
+      print 'Trait.'
+      Trait.propagate_id(fk: 'predicate_term_id', other: 'terms.id', set: 'predicate_term_uri', with: 'uri')
+      print '.'
+      Trait.propagate_id(fk: 'object_term_id', other: 'terms.id', set: 'object_term_uri', with: 'uri')
+      print '.'
+      Trait.propagate_id(fk: 'units_term_id', other: 'terms.id', set: 'units_term_uri', with: 'uri')
+      print '.'
+      Trait.propagate_id(fk: 'statistical_method_term_id', other: 'terms.id', set: 'statistical_method_term_uri', with: 'uri')
+      print '.'
+      Trait.propagate_id(fk: 'sex_term_id', other: 'terms.id', set: 'sex_term_uri', with: 'uri')
+      print '.'
+      Trait.propagate_id(fk: 'lifestage_term_id', other: 'terms.id', set: 'lifestage_term_uri', with: 'uri')
+      puts "\n"
       print 'MetaTrait.'
-      # MetaTrait.propagate_id(fk: 'predicate_term_id', other: 'terms.id', set: 'predicate_term_uri', with: 'uri')
-      # print '.'
-      # MetaTrait.propagate_id(fk: 'object_term_id', other: 'terms.id', set: 'object_term_uri', with: 'uri')
-      # print '.'
-      # MetaTrait.propagate_id(fk: 'units_term_id', other: 'terms.id', set: 'units_term_uri', with: 'uri')
-      # print '.'
-      # MetaTrait.propagate_id(fk: 'statistical_method_term_id', other: 'terms.id', set: 'statistical_method_term_uri', with: 'uri')
+      MetaTrait.propagate_id(fk: 'predicate_term_id', other: 'terms.id', set: 'predicate_term_uri', with: 'uri')
+      print '.'
+      MetaTrait.propagate_id(fk: 'object_term_id', other: 'terms.id', set: 'object_term_uri', with: 'uri')
+      print '.'
+      MetaTrait.propagate_id(fk: 'units_term_id', other: 'terms.id', set: 'units_term_uri', with: 'uri')
+      print '.'
+      MetaTrait.propagate_id(fk: 'statistical_method_term_id', other: 'terms.id', set: 'statistical_method_term_uri', with: 'uri')
       puts "\n"
 
       # Aaaaaactually, I think I'll delete the _ids in a second migration after I'm happy with these.
