2ddc72d7f52fde4b3f0d98201ced69cb2a88c239
JRice <jrice@eol.org>
2021-01-22 14:22:46 -0500

    Adding a little logic to MetaXmlField.load.

--- app/models/meta_xml_field.rb
+++ app/models/meta_xml_field.rb
@@ -2,17 +2,25 @@
 class MetaXmlField < ApplicationRecord
   class << self
     def load
-      # delete_all
       filename = Rails.root.join('db', 'data', 'meta_analyzed.json')
       raise("File missing: #{filename}") unless File.exist?(filename)
-      
+
       data = JSON.parse(File.read(filename))
+      puts "I see #{data.size} items."
+
+
+      # remove things from the list if we already know about them:
       have = {}
       all.each do |mxf|
         have["#{mxf.for_format}:#{mxf.term}"] = true
       end
       data.delete_if { |d| have.key?("#{d['for_format']}:#{d['term']}") }
-      return nil if data.empty?
+
+      puts "After removing known fields, I now have #{data.size} items."
+      if data.empty?
+        puts "** NOTHING NEW TO ADD. No action taken."
+        return nil
+      end
 
       puts "** ADDING #{data.size} new meta xml fields"
       import!(data, on_duplicate: :ignore)
