08b7f51b03919bb8b40261fee975146ed167d58b
JRice <jrice@eol.org>
2021-03-01 09:51:26 -0500

    Fixing a typo and some IDs from migrations to match beta, where they ran successfully.

--- db/migrate/20210224161724_remove_harvest_id_from_formats.rb
+++ db/migrate/20210224161724_remove_harvest_id_from_formats.rb
@@ -1,4 +1,4 @@
-class RemoveHarevstIdFromFormats < ActiveRecord::Migration[5.2]
+class RemoveHarvestIdFromFormats < ActiveRecord::Migration[5.2]
   def up
     Format.where('harvest_id IS NOT NULL').delete_all
     remove_column :formats, :harvest_id
