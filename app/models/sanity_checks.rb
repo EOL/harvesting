019b41a0a37a2fe50c7512e1f02040c6b028fffa
Michael Vitale <mmvitale@gmail.com>
2021-03-19 16:07:26 -0400

    check for duplicate assocs as well as traits

--- app/models/sanity_checks.rb
+++ app/models/sanity_checks.rb
@@ -1,4 +1,22 @@
 class SanityChecks
+  TRAIT_COMPARISON_COLS = [
+    'node_id', 
+    'predicate_term_uri',
+    'object_term_uri',
+    'statistical_method_term_uri',
+    'sex_term_uri',
+    'lifestage_term_uri',
+    'measurement',
+    'literal'
+  ]
+
+  ASSOC_COMPARISON_COLS = [
+    'predicate_term_uri',
+    'node_id',
+    'target_node_id',
+    'sex_term_uri',
+    'lifestage_term_uri'
+  ]
 
   def initialize(harvest, process)
     @harvest = harvest
@@ -7,58 +25,54 @@ class SanityChecks
 
   def perform_all
     check_for_duplicate_traits
+    check_for_duplicate_assocs
   end
 
   def check_for_duplicate_traits
-    all_count = @harvest.traits.count
-    uniq_count = @harvest.traits.count(
-      <<~SQL
-        DISTINCT(
-        concat_ws(',',
-          coalesce(node_id, 'n'), 
-          coalesce(predicate_term_uri, 'n'),
-          coalesce(object_term_uri, 'n'),
-          coalesce(statistical_method_term_uri, 'n'),
-          coalesce(sex_term_uri, 'n'), 
-          coalesce(lifestage_term_uri, 'n'), 
-          coalesce(measurement, 'n'), 
-          coalesce(literal, 'n')
-        ))
-      SQL
-    )
-
-    diff = @harvest.traits.count - uniq_count 
-
-    unless diff == 0
-      @process.log("DUPLICATE TRAITS FOUND! There are only #{uniq_count} (of #{all_count} total) unique traits.")
-      log_duplicate_trait_ids
-    end
+    check_for_duplicates(Trait, 'traits', 'trait', TRAIT_COMPARISON_COLS)
   end
 
-  def log_duplicate_trait_ids
-    trait_id_q = <<~SQL
+  def check_for_duplicate_assocs
+    check_for_duplicates(Assoc, 'assocs', 'association', ASSOC_COMPARISON_COLS)
+  end
+
+  def log_duplicates(klass, table, name, cols)
+    q = <<~SQL
       SELECT t1.resource_pk, t1.id, t2.resource_pk, t2.id FROM
-      traits t1 JOIN traits t2
+      #{table} t1 JOIN #{table} t2
       ON 
-      coalesce(t1.node_id, 'n') = coalesce(t2.node_id, 'n') AND
-      coalesce(t1.predicate_term_uri, 'n') = coalesce(t2.predicate_term_uri, 'n') AND
-      coalesce(t1.object_term_uri, 'n') = coalesce(t2.object_term_uri, 'n') AND 
-      coalesce(t1.statistical_method_term_uri, 'n') = coalesce(t2.statistical_method_term_uri, 'n') AND
-      coalesce(t1.sex_term_uri, 'n') = coalesce(t2.sex_term_uri, 'n') AND
-      coalesce(t1.lifestage_term_uri, 'n') = coalesce(t2.lifestage_term_uri, 'n') AND 
-      coalesce(t1.measurement, 'n') = coalesce(t2.measurement, 'n') AND
-      coalesce(t1.literal, 'n') = coalesce(t2.literal, 'n') AND
+      #{cols.map { |c| "coalesce(t1.#{c}, 'null') = coalesce(t2.#{c}, 'null')" }.join("AND\n")} AND
       t1.id < t2.id AND
       t1.harvest_id = #{@harvest.id} AND t2.harvest_id = #{@harvest.id}
       LIMIT 100
     SQL
 
-    result = Trait.connection.execute(trait_id_q)
+    result = klass.connection.execute(q)
 
-    @process.log("Duplicate trait pairs (up to 100):") 
+    @process.log("Duplicate #{name} pairs (up to 100):") 
     
     result.each do |r|
       @process.log("(resource_pk: #{r[0]}, id: #{r[1]}), (resource_pk: #{r[2]}, id: #{r[3]})")
     end
   end
+
+  def check_for_duplicates(klass, table, name, cols)
+    all_count = @harvest.send(table).count
+    uniq_count = @harvest.send(table).count(
+      <<~SQL
+        DISTINCT(
+        concat_ws(',',
+          #{cols.map { |c| "coalesce(#{c}, 'null')" }.join(",\n")}
+        ))
+      SQL
+    )
+
+    diff = all_count - uniq_count 
+
+    unless diff == 0
+      @process.log("DUPLICATE TRAITS FOUND! There are only #{uniq_count} (of #{all_count} total) unique #{name}s.")
+      log_duplicates(klass, table, name, cols)
+    end
+  end
 end
+
