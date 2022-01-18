e94ad03c6a7fc1bcfa6437d81587dd0032694a39
Jeremy Rice (Win box) <jrice@eol.org>
2022-01-18 10:54:01 -0500

    Added md5_hash to the downloaded_urls table

--- app/models/downloaded_url.rb
+++ app/models/downloaded_url.rb
@@ -1,4 +1,14 @@
 # Just a unique identifier to help us build a single place to store images that we download from URLs.
 class DownloadedUrl < ApplicationRecord
   belongs_to :medium
+  before_save :hash_url
+
+  def self.by_url(url)
+    md5_hash = Digest::MD5.hexdigest(self[:url])
+    self.find_by_md5_hash(md5_hash)
+  end
+
+  def hash_url
+    self[:md5_hash] = Digest::MD5.hexdigest(self[:url])
+  end
 end
--- db/migrate/20220103001556_create_downloaded_urls.rb
+++ db/migrate/20220103001556_create_downloaded_urls.rb
@@ -3,16 +3,18 @@ class CreateDownloadedUrls < ActiveRecord::Migration[5.2]
     create_table :downloaded_urls do |t|
       t.integer :resource_id
       t.string :url, limit: 2083
+      t.string :md5_hash, limit: 32
 
       t.timestamps
     end
     add_index :downloaded_urls, :resource_id
-    add_index :downloaded_urls, :url
+    add_index :downloaded_urls, :md5_hash
     add_column :media, :downloaded_url_id, :integer
     downloaded_urls = []
     Medium.where('source_url IS NOT NULL').find_each do |medium|
       # TODO: test whether import! can actually set the PK id. :S
-      downloaded_urls << DownloadedUrl.new(id: medium.id, resource_id: medium.resource_id, url: medium.source_url)
+      downloaded_urls << DownloadedUrl.new(id: medium.id, resource_id: medium.resource_id, url: medium.source_url,
+        md5_hash: Digest::MD5.hexdigest(medium.source_url))
     end
     DownloadedUrl.import!(downloaded_urls)
     Medium.update_all("downloaded_url_id = id")
