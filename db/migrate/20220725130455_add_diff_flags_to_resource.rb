78843d2926632e7b2355e3e4bf75c1acfb11afdd
Jeremy Rice (Win box) <jrice@eol.org>
2022-08-02 08:02:00 -0400

    Ooops, wrong migration name

--- db/migrate/20220725130455_add_diff_flags_to_resource.rb
+++ db/migrate/20220725130455_add_diff_flags_to_resource.rb
@@ -1,5 +1,5 @@
 # 20220412125710
-class RenameResourcesPublishStatus < ActiveRecord::Migration[5.2]
+class AddDiffFlagsToResource < ActiveRecord::Migration[5.2]
   def change
     add_column :resources, :requires_full_reharvest_after, :datetime, null: true, default: nil
   end
