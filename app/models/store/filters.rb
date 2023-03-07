42f51379ddfca6aee1441649def8658a8ff61e2f
Jeremy Rice (Win box) <jrice@eol.org>
2023-03-07 10:00:03 -0500

    Fixing a bug with Store::Filters.remove_emojis

--- app/models/store/filters.rb
+++ app/models/store/filters.rb
@@ -2,7 +2,8 @@ module Store
   # These are just methods that can be called by several other "Store" modules.
   module Filters
     def remove_emojis(val)
-      val.gsub(EmojiRegex::Text, '')
+      # NOTE: this gives a deprecated constant warning, but I swear it's really not: https://github.com/ticky/ruby-emoji-regex
+      val.gsub(EmojiRegex::Regex, '')
     end
   end
 end
