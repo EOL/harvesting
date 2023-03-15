9d600059a8ea98bba89f86a39914af3d3bbf7e73
Jeremy Rice (Win box) <jrice@eol.org>
2023-03-15 12:21:17 -0400

    Remove italics in addition to emojis

--- app/models/store/filters.rb
+++ app/models/store/filters.rb
@@ -2,7 +2,8 @@ module Store
   # These are just methods that can be called by several other "Store" modules.
   module Filters
     def remove_emojis(val)
-      val.gsub(Unicode::Emoji::REGEX, '')
+      # The second set removes italicized characters, which are also not allowed in the DB.
+      val.gsub(Unicode::Emoji::REGEX, '').gsub(/[\u{1d600}-\u{1d6ff}]/, '')
     end
   end
 end
