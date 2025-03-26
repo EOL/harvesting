5d168e16ddcd58597ef70ea63fa92ff744062e6c
JRice (Ubuntu Tower) <jrice@eol.org>
2025-03-26 13:20:08 -0400

    Simpler prometheus attempt

--- config/initializers/prometheus.rb
+++ config/initializers/prometheus.rb
@@ -1,23 +1,7 @@
 require 'prometheus/client'
-require 'prometheus/client/rack/collector'
-require 'prometheus/client/rack/exporter'
 
-# Create a default registry
-Prometheus::Client.registry = Prometheus::Client::Registry.new
+# returns a default registry
+prometheus = Prometheus::Client.registry
 
-# Just some very basic stuff for testing:
-RESOURCE_REQUESTS = Prometheus::Client.registry.counter(
-  :harvesting_resource_requests_total,
-  docstring: 'A counter of resource requests',
-  labels: [:abbr]
-)
-
-RESOURCE_RENDER_DURATION = Prometheus::Client.registry.histogram(
-  :harvesting_resource_query_seconds,
-  docstring: 'Page render duration for resources',
-  labels: [:abbr]
-)
-
-# Add basic app metrics
-Rails.application.middleware.use Prometheus::Client::Rack::Collector
-Rails.application.middleware.use Prometheus::Client::Rack::Exporter
+# create a new counter metric
+http_requests = prometh
